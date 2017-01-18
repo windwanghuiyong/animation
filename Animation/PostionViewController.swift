d4d091a487cf834eda845a6466c240b5863d1f8d
wanghuiyong <weifeng19911024@163.com>
2017-01-18 16:38:29 +0800

    add swift file

---
+++ Animation/PostionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostionViewController.swift
+//  Animation
+//
+//  Created by wanghuiyong on 08/01/2017.
+//  Copyright © 2017 wanghuiyong. All rights reserved.
+//
+
+import UIKit
+
+class PostionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
